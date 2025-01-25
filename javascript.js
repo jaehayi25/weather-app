abee0cade54c3e6a3dc499d88a3870420f87f93c
Jaeha Yi <jaehaha1@gmail.com>
2025-01-25 00:01:10 -0700

    correctly updating weather block after submitting location

--- javascript.js
+++ javascript.js
@@ -1,23 +1,19 @@
 const img = document.querySelector('img');
 
-function getData(location) {
-    fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=K2A3SAV8EL5GKDWHDYFYVBLR5`, {mode: 'cors'})
+async function getData(location) {
+    return fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=K2A3SAV8EL5GKDWHDYFYVBLR5`, {mode: 'cors'})
     .then(function(response) {
         //console.log(response.json()); 
         return response.json(); 
     })
     .then(function(response) {
-        console.log(response); 
-        processData(response); 
+        return processData(response); 
     }); 
 }
 
 function processData(jsonData) {
     // returns object with only required data (Fahrenheit, Celcius, weather conditions - current and trends)
-    console.log(jsonData.currentConditions); 
-    console.log(jsonData.currentConditions.temp); 
-    console.log(jsonData.currentConditions.conditions); 
-    console.log(jsonData.description); 
+    return [jsonData.currentConditions.temp, jsonData.currentConditions.conditions, jsonData.description]; 
 }
 
 // getData("london")
@@ -29,10 +25,15 @@ document.getElementById('weatherForm').addEventListener('submit', function(e) {
     const location = document.getElementById('location').value;
     
     console.log('Fetching weather for location:', location);
-    getData(location); 
     
     const resultDiv = document.getElementById('result');
     resultDiv.style.display = 'block';
+
+    getData(location).then(result => {
+        document.getElementsByClassName('temperature')[0].textContent = `${result[0]}°`;
+        document.getElementsByClassName('weather-type')[0].textContent = result[1];
+        document.getElementsByClassName('forecast')[0].textContent = result[2];    
+    }); 
     // resultDiv.textContent = `Fetching weather data for ${location}...`;
     
     document.getElementById('location').value = '';
